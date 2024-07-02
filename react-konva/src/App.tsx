45470d4d4e1cd537de04370be0645e7b7b26a72c
m208 <avkomissarov@gmail.com>
2024-07-03 01:13:18 +0300

    highlight shapes on hover

--- react-konva/src/App.tsx
+++ react-konva/src/App.tsx
@@ -1,5 +1,8 @@
-import { Stage, Layer, Rect, Circle } from 'react-konva';
+import { Stage, Layer, Rect, Circle, Line, } from 'react-konva';
 import './App.css'
+import { Shape } from 'konva/lib/Shape';
+
+const defaultColor = '#00D2FF';
 
 function App() {
   return (
@@ -8,11 +11,33 @@ function App() {
     // Rect and Circle are not DOM elements. They are 2d shapes on canvas
 
     <Stage width={window.innerWidth} height={window.innerHeight}>
-      <Layer>
-        <Rect width={50} height={50} fill="red" />
-        <Circle x={200} y={200} stroke="black" radius={50} />
+      <Layer
+        onMouseOver={(evt) => {
+          const shape = evt.target as Shape;
+          document.body.style.cursor = 'pointer';
+          shape.fill('red');
+        }}
+        onMouseOut={(evt) => {
+          const shape = evt.target as Shape;
+          document.body.style.cursor = 'default';
+          shape.fill(defaultColor);
+        }}
+      >
+        <Rect width={50} height={50} fill={defaultColor} name='Figure 1 RECT' />
+        <Circle x={200} y={200} stroke="black" fill={defaultColor} radius={50} name='Figure 2 Circle' />
+        <Line
+          points={[23, 20, 23, 160, 70, 93, 150, 109, 290, 139, 270, 93]}
+          fill={defaultColor}
+          stroke={'black'}
+          strokeWidth={1}
+          closed={true}
+          tension={0.3}
+          opacity={0.7}
+          name='Figure 3 BLOB'
+        >
+        </Line>
       </Layer>
-    </Stage>
+    </Stage >
 
   )
 }
