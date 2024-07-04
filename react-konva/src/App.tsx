3306d539da628a33d02ed5c410a50e394bfd6b53
m208 <avkomissarov@gmail.com>
2024-07-04 23:07:44 +0300

    zooming stage relative to pointer position

--- react-konva/src/App.tsx
+++ react-konva/src/App.tsx
@@ -3,6 +3,8 @@ import { Stage } from 'react-konva';
 import { LayerOne } from './components/LayerOne/LayerOne';
 import { LayerTwo } from './components/LayerTwo/LayerTwo';
 
+const scaleBy = 1.1;
+
 function App() {
 
   return (
@@ -10,7 +12,37 @@ function App() {
     // Layer - is an actual 2d canvas element, so you can have several layers inside the stage
     // Rect and Circle are not DOM elements. They are 2d shapes on canvas
 
-    <Stage width={window.innerWidth} height={window.innerHeight} draggable={true}>
+    <Stage
+      width={window.innerWidth}
+      height={window.innerHeight}
+      draggable={true}
+      onWheel={(e) => {
+        e.evt.preventDefault();
+
+        const stage = e.target.getStage();
+        if (!stage) return
+
+        const oldScale = stage.scaleX();
+
+        const direction = e.evt.deltaY > 0 ? 1 : -1;
+        const newScale = direction > 0 ? oldScale * scaleBy : oldScale / scaleBy;
+
+        stage.scale({ x: newScale, y: newScale });
+
+        const pointer = stage.getPointerPosition();
+        if (!pointer) return
+        const mousePointTo = {
+          x: (pointer.x - stage.x()) / oldScale,
+          y: (pointer.y - stage.y()) / oldScale,
+        };
+        const newPos = {
+          x: pointer.x - mousePointTo.x * newScale,
+          y: pointer.y - mousePointTo.y * newScale,
+        };
+        stage.position(newPos);
+
+      }}
+    >
       <LayerOne></LayerOne>
       <LayerTwo></LayerTwo>
     </Stage >
