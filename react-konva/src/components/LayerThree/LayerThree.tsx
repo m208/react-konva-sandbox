b8a261ddae37fbe270c7a8a9a41154a88cd03634
m208 <avkomissarov@gmail.com>
2024-08-06 21:34:01 +0300

    Node.isClientRectOnScreen usage

--- react-konva/src/components/LayerThree/LayerThree.tsx
+++ react-konva/src/components/LayerThree/LayerThree.tsx
@@ -1,4 +1,5 @@
-import { forwardRef, useImperativeHandle, useState } from "react";
+import Konva from "konva";
+import { forwardRef, useImperativeHandle, useRef, useState } from "react";
 import { Layer, Image as ReactImage, Text } from "react-konva";
 import useImage from 'use-image';
 
@@ -11,9 +12,16 @@ export type StageDragHandler = {
 export const LayerThree = forwardRef<StageDragHandler, LayerProps>((props, ref) => {
     const [counter, setCounter] = useState(0);
 
+    const imageRef = useRef<Konva.Image>(null);
+
     useImperativeHandle(ref, () => ({
         onDragEnd() {
-            console.log('some action');
+            console.log(
+                imageRef.current?.isClientRectOnScreen()
+                    ? 'Image on the screen'
+                    : 'Image out of the screen'
+            );
+
             setCounter(prev => prev + 1);
         },
     }));
@@ -21,13 +29,10 @@ export const LayerThree = forwardRef<StageDragHandler, LayerProps>((props, ref) 
     const [image] = useImage(`https://res.cloudinary.com/dodpx3cj9/image/upload/f_auto,q_auto/v1/mockup-application/oipwr4jv0pdxgyq3qxx0`);
 
     return (
-        <>
-            <Layer>
-                <Text text={'' + counter}></Text>
-                <ReactImage draggable={true} image={image} x={100} y={100}></ReactImage>
-            </Layer >
-        </>
-
+        <Layer>
+            <Text text={'' + counter}></Text>
+            <ReactImage draggable={true} ref={imageRef} image={image} x={100} y={100}></ReactImage>
+        </Layer >
     )
 })
 
